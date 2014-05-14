03792ca38599f74e8df82f90a124734e637eb2ce
Akshay Naik <devnull@localhost>
2014-05-14 22:10:08 +0530

    colgadmin dropdown menu added.

--- markup/topbar_basic.php
+++ markup/topbar_basic.php
@@ -1,15 +1,7 @@
 <div id='topbarright'>
 	<?php echo "Hello ".$_SESSION['fullname']; ?> |
 	<a class='topbar' href='colgmainpage.php?colgcode=<?php echo $_SESSION['collegecode'] ?>'><div class='topnav'>MY COLLEGE</div></a>|
-	<div class="dropdwn">
-	<a class='topbar' href=''>
-	<div class='topnav'>ACCOUNT</div>
-	</a>
-	<ul>
-		<li>OPT 1</li>
-		<li>OPT 2</li>
-	</ul>
-	</div> 
+	<a class='topbar' href=''><div class='topnav'>ACCOUNT</div></a>
 	|<a class='topbar' href='proc/userlogout.php'><div class='topnav' id='logoutbtn'>LOGOUT</div></a>|
 	<a class='searchbtn topbar' href='qsearchpage.php'><div class='topnav'>SEARCH</div></a>
 </div> 
--- markup/topbar_colgadmin.php
+++ markup/topbar_colgadmin.php
@@ -1,8 +1,16 @@
 <div id='topbarright' style='float:right;word-spacing:0;'>
-	<?php echo "Hello ".$_SESSION['fullname']; ?> |
-	<a class='topbar' href='colgmainpage.php?colgcode=<?php echo $_SESSION['collegecode'] ?>'><div class='topnav'>MY COLLEGE</div></a>|
-	<a class='topbar' href=''><div class='topnav'>ACCOUNT</div></a>|
-	<a class='topbar' href='proc/userlogout.php'><div class='topnav' id='logoutbtn'>LOGOUT</div></a>|
-	<a class='searchbtn topbar' href='qsearchpage.php'><div class='topnav'>SEARCH</div></a>
+	<?php echo "Hello ".$_SESSION['fullname']; ?> |<div class="dropdwn">
+	<a class='topbar' href='colgmainpage.php?colgcode=<?php echo $_SESSION['collegecode'];?>'><div class='topnav'>MY COLLEGE</div></a>
+	<ul>
+		<a class="dropdwn" href="editaboutcolg.php?colgcode=<?php echo $_SESSION['collegecode'];?>"><li class="dropdwn" style=''><div>EDIT INFO</div></li></a>
+		<a class="dropdwn" href="writenewpost.php?colgcode=<?php echo $_SESSION['collegecode'];?>"><li><div>NEW POST</div></li></a>
+	</ul>
+	</div>|<a class='topbar' href=''>
+	<div class='topnav'>ACCOUNT</div>
+	</a>|<a class='topbar' href='proc/userlogout.php'>
+	<div class='topnav' id='logoutbtn'>LOGOUT</div>
+	</a>|<a class='searchbtn topbar' href='qsearchpage.php'>
+	<div class='topnav'>SEARCH</div>
+	</a>
 
 </div> 
