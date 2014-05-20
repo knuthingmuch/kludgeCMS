039a0fd07125721c10c4190740a4a7b011c40732
Akshay Naik <devnull@localhost>
2014-05-21 02:33:36 +0530

    User feedback and security fixes for setcolgadmin

--- proc/colgadminset.php
+++ proc/colgadminset.php
@@ -3,29 +3,47 @@ session_start();
 require_once '../lib/mysql-lib.php';
 require_once '../lib/users-lib.php';
 
-$CONN = db_sysconnect();
-
-if(isset($_POST['setuname']))
+if(userIsColgAdmin())
 {
-	$uname=mysqli_real_escape_string($CONN,$_POST['setuname']);
-	$result = mysqli_query($CONN,"SELECT collegecode FROM users WHERE uname='".$uname."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
-	$result=mysqli_fetch_array($result);
-	if(userIsColgAdmin() and $_SESSION['collegecode']==$result['collegecode'])		//can only set user from his own colg as co-admin.
-	{
-		mysqli_query($CONN,"UPDATE users SET utype='CADMIN' WHERE uname='".$uname."'") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
-	}
-}
-else if(isset($_POST['removeuname']))
-{
-	$uname=mysqli_real_escape_string($CONN,$_POST['removeuname']);
-	$result = mysqli_query($CONN,"SELECT collegecode FROM users WHERE uname='".$uname."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
-	$result=mysqli_fetch_array($result);
-	if(userIsColgAdmin() and $_SESSION['collegecode']==$result['collegecode'])		//can only set user from his own colg as co-admin.
-	{
-		mysqli_query($CONN,"UPDATE users SET utype='BASIC' WHERE uname='".$uname."'") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
-	}
-}
+	$CONN = db_sysconnect();
 
-db_sysclose($CONN);
-header('location: '.$_SERVER['HTTP_REFERER']);
+	if(isset($_POST['setuname']))
+	{
+		$uname=mysqli_real_escape_string($CONN,$_POST['setuname']);
+		$result = mysqli_query($CONN,"SELECT uid,collegecode FROM users WHERE uname='".$uname."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
+		$row=mysqli_fetch_array($result);
+		if(mysqli_num_rows($result)==0 or isSiteAdmin($row['uid']))
+			$msgcode=1;
+		else
+		{
+			mysqli_query($CONN,"UPDATE users SET utype='CADMIN' WHERE uname='".$uname."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
+		}
+	}
+	else if(isset($_POST['removeuname']))
+	{
+		$uname=mysqli_real_escape_string($CONN,$_POST['removeuname']);
+		$result = mysqli_query($CONN,"SELECT uid,collegecode FROM users WHERE uname='".$uname."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
+		$row=mysqli_fetch_array($result);
+		if($uname!=$_SESSION['uname'])	//can't unset yourself.
+		{
+			if(mysqli_num_rows($result)==0 or isSiteAdmin($row['uid']))
+				$msgcode=1;
+			else
+			{
+				if($_SESSION['collegecode']==$row['collegecode'])		//can only set user from his own colg as co-admin.
+				{
+					mysqli_query($CONN,"UPDATE users SET utype='BASIC' WHERE uname='".$uname."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
+				}
+				else
+					$msgcode=1;
+			}
+		}
+		else
+			$msgcode=2;
+	}
+	db_sysclose($CONN);
+	header('location: ../setcolgadmin.php?colgcode='.$_SESSION['collegecode'].'&msgcode='.$msgcode);
+}
+else
+	header('location: '.$_SERVER['HTTP_REFERER']) or header('location: ../index.php');
 ?>
--- setcolgadmin.php
+++ setcolgadmin.php
@@ -8,7 +8,7 @@ if(isset($_GET['colgcode']))
 	if(userIsColgAdmin() and $_GET['colgcode']==$_SESSION['collegecode'])
 	{
 ?>
-	<span>Note: All college admins may post under their own college sections but cannot edit each others posts.</span>
+	<span>All college admins may post under their own college sections but cannot edit each others posts.</span>
 	<br/><br/>
 	<form action="proc/colgadminset.php" method="post">
 	<span>Set the following user as your college co-administrator.</span>
@@ -23,19 +23,29 @@ if(isset($_GET['colgcode']))
 	Username: <input type="text" name="removeuname">
 	<input type="submit" value="Unset">
 	</form>
+<?php
+	if(isset($_GET['msgcode']))
+	{
+		if($_GET['msgcode']==1)
+			echo "The username you typed is invalid.";
+		else if($_GET['msgcode']==2)
+			echo "You can't unset youself, ask another admin to do so.";
+	}
+?>
+	<br/>
+	<span>Note: These changes won't take effect until the specified user logs out.</span>
+	<br/>
 	<hr/>
 	<br/><br/>
-	<span>Your college co-administrators are:</span>
-	<br/>
 	<table id="adminlist">
-	<tr>
-	<th>Username</th><th>Full name</th>
-	</tr>
+	
 <?php
 	$CONN = db_sysconnect();
 	$result = mysqli_query($CONN,"SELECT uname,fullname FROM users WHERE uid!=".$_SESSION['uid']." and utype='CADMIN' and collegecode='".$_SESSION['collegecode']."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));	//own name not displayed
+	
 	if(mysqli_num_rows($result)!=0)
 	{
+		echo "<span>Your college co-administrators are:</span><tr><th>Username</th><th>Full name</th></tr>";
 		while($row = mysqli_fetch_array($result))
 		{
 			echo "<tr><td>".$row['uname']."</td><td>".$row['fullname']."</td></tr>";
@@ -43,8 +53,24 @@ if(isset($_GET['colgcode']))
 		echo "</table>";
 	}
 	else
-		echo "<tr><td>-----</td><td>-----</td></tr></table>You are the only admin from your college.";
+		echo "You are the only admin from your college.";
 
+	$result = mysqli_query($CONN,"SELECT COUNT(*) as count FROM users WHERE collegecode='".$_SESSION['collegecode']."';") or systemlog($_SERVER['PHP_SELF']."  SQL query error: ".mysqli_error($CONN));
+	$result = mysqli_fetch_array($result);
+	db_sysclose($CONN);
+?>
+	</table>
+	<hr/>
+	Total registered users from your college: <?php echo $result['count']; ?>
+<?php
+	}
+	else
+	{
+	?>
+		<div id="error">
+		You do not have the priviledges required to access this page or requested resource does not exist.
+		</div>
+<?php
 	}
 }
 
