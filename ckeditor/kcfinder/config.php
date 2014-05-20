3e5d73603fc3f0ab750dd2eb5e4647ab71443eb4
Akshay Naik <devnull@localhost>
2014-05-20 20:06:09 +0530

    rev 23: Added file upload features for admins and set colg admin.

--- ckeditor/kcfinder/config.php
+++ ckeditor/kcfinder/config.php
@@ -22,7 +22,7 @@ $_CONFIG = array(
 // GENERAL SETTINGS
 
     'disabled' => true,
-    'uploadURL' => "../../uploads", //default, each colg has own dir.
+    'uploadURL' => "../../uploads", //default for siteadmin, each colg has own dir.
     'uploadDir' => "",
     'theme' => "default",
 
